b0c04caf78483bdba6ec50be6d005bbd981cc804
Lex Robinson <lex@lex.me.uk>
2019-09-14 19:20:24 +0100

    Don't render the canvas until the fonts are loaded
    This allows us to simplify our assumptions, because the widths of things
    aren't going to change suddenly between startup and the current frame.
    
    Also, adding new font weights can be done in code rather than in HTML
    which is nice?

--- src/display.ts
+++ src/display.ts
@@ -1,30 +1,70 @@
 import { Clock } from "@/display/clock";
 import { WIDTH, HEIGHT } from "@/display/constants";
+import { loadFonts } from "@/display/fonts";
+import { join } from "path";
 
-let canvas = document.getElementById("root") as HTMLCanvasElement;
-if (!canvas) {
-	throw new Error("gimme me my canvas");
+function getCtx(): CanvasRenderingContext2D {
+	let canvas = document.getElementById("root") as HTMLCanvasElement;
+	if (!canvas) {
+		throw new Error("gimme me my canvas");
+	}
+	let ctx = canvas.getContext("2d");
+	if (!ctx) {
+		throw new Error("??");
+	}
+
+	return ctx;
 }
-let ctx = canvas.getContext("2d")!;
-if (!ctx) {
-	throw new Error("??");
-}
 
-let TEMPend = performance.now() + 20 * 60 * 1000;
-
-function anime(now: DOMHighResTimeStamp): void {
-	ctx.clearRect(0, 0, WIDTH, HEIGHT);
-
+function showLoading(ctx: CanvasRenderingContext2D): void {
 	ctx.save();
-	ctx.translate(0, HEIGHT / 2);
-	Clock(ctx, Math.max(0, TEMPend - now));
+	ctx.font = "300px 'comic sans ms'";
+	ctx.textAlign = "center";
+	ctx.textBaseline = "middle";
+	ctx.fillText("Loading!!1", WIDTH / 2, HEIGHT / 2);
 	ctx.restore();
+}
 
-	ctx.strokeStyle = "red";
-	ctx.moveTo(0, HEIGHT / 2);
-	ctx.lineTo(WIDTH, HEIGHT / 2);
-	ctx.stroke();
+function showLoadingFailure(ctx: CanvasRenderingContext2D, error: Error): void {
+	console.error("Loady fail: ", error);
+	ctx.clearRect(0, 0, WIDTH, HEIGHT);
+	ctx.font = "300px 'comic sans ms'";
+	ctx.textAlign = "center";
+	ctx.textBaseline = "middle";
+	ctx.fillStyle = "red";
+	ctx.fillText("Failed to load :(", WIDTH / 2, HEIGHT / 2, WIDTH);
+}
+
+async function main(): Promise<void> {
+	let fontsLoaded = loadFonts();
+
+	let ctx = getCtx();
+
+	try {
+		showLoading(ctx);
+		await fontsLoaded;
+	} catch (e) {
+		showLoadingFailure(ctx, e);
+		return;
+	}
+
+	let TEMPend = performance.now() + 20 * 60 * 1000;
+
+	function anime(now: DOMHighResTimeStamp): void {
+		ctx.clearRect(0, 0, WIDTH, HEIGHT);
+
+		ctx.save();
+		ctx.translate(0, HEIGHT / 2);
+		Clock(ctx, Math.max(0, TEMPend - now));
+		ctx.restore();
+
+		ctx.strokeStyle = "red";
+		ctx.moveTo(0, HEIGHT / 2);
+		ctx.lineTo(WIDTH, HEIGHT / 2);
+		ctx.stroke();
+		window.requestAnimationFrame(anime);
+	}
+
 	window.requestAnimationFrame(anime);
 }
-
-window.requestAnimationFrame(anime);
+main();
--- src/display/fonts.ts
+++ src/display/fonts.ts
@@ -1,5 +1,29 @@
 const FONT = "Orbitron";
+type OrbitronWeight = 400;
+const EXPECTED_WEIGHTS: OrbitronWeight[] = [400];
 
-export function fontify(size: string): string {
-	return `${size} ${FONT}`;
+// WebFont.load
+export function fontify(size: string, weight: OrbitronWeight = 400): string {
+	return `${weight} ${size} ${FONT}`;
+}
+
+export function loadFonts(): Promise<void> {
+	return new Promise((resolve, reject): void => {
+		let variants = EXPECTED_WEIGHTS.map(
+			(weight) => `n${Math.floor(weight / 100)}`,
+		).join(",");
+
+		window.WebFont.load({
+			classes: false,
+			active() {
+				resolve();
+			},
+			inactive() {
+				reject();
+			},
+			google: {
+				families: [`${FONT}:${variants}`],
+			},
+		});
+	});
 }
