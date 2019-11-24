128dd2dde526dce4d0cedb43330eb1244debda9b
Lex Robinson <lex@lex.me.uk>
2019-11-24 13:44:20 +0000

    Add basic typing animation for phase changes
    I couldn't think of anything better for this one, other than the
    rediculous skuomorphism of rotating the text in 3D like a roll display
    (which I also couldn't work out how to do in canvas) so I went with
    a tried and tested typewriter effect.
    
    I'd like for this to have a cursor and occasionally make a typo, but for
    now it'll do great.

--- src/display/phase.ts
+++ src/display/phase.ts
@@ -8,16 +8,86 @@ const TOP_PADDING = 85;
 
 const MAX_WIDTH = WIDTH - LEFT_PADDING - RIGHT_PADDING;
 
-export class PhaseTracker extends BaseCanvasItem {
-	public phase = "Setup Phase";
+enum Doing {
+	IN,
+	OUT,
+	STATIC,
+}
 
-	render(ctx: CanvasRenderingContext2D): void {
+const DELETING_SPEED = 50;
+const TYPING_SPEED = 100;
+
+export class PhaseTracker extends BaseCanvasItem {
+	private mode: Doing = Doing.STATIC;
+
+	// constructor(ctx: CanvasRenderingContext2D) {
+	// 	super(ctx);
+	// }
+	private actionTimer = 0;
+	private _phase = "";
+	private cPhase = "";
+	public get phase(): string {
+		return this._phase;
+	}
+	public set phase(newP: string) {
+		if (newP == this._phase) {
+			return;
+		}
+		if (!this._phase) {
+			// First phase, typewriter it all in
+			this.mode = Doing.IN;
+			this.cPhase = "";
+			this.actionTimer = 0;
+		} else {
+			if (this.mode == Doing.STATIC) {
+				// Just in case it isn't already for some reason
+				this.cPhase = this._phase;
+				this.actionTimer = 0;
+			}
+			this.mode = Doing.OUT;
+		}
+		this._phase = newP;
+	}
+
+	private doTyping(ft: DOMHighResTimeStamp): void {
+		if (this.mode == Doing.STATIC) {
+			return;
+		}
+		this.actionTimer += ft;
+		if (this.mode == Doing.OUT) {
+			if (this.actionTimer < DELETING_SPEED) {
+				return;
+			}
+			this.actionTimer = 0;
+			this.cPhase = this.cPhase.substr(0, this.cPhase.length - 1);
+			if (this.cPhase == "") {
+				this.mode = Doing.IN;
+			}
+		} else {
+			if (this.actionTimer < TYPING_SPEED) {
+				return;
+			}
+			this.actionTimer = 0;
+			this.cPhase = this._phase.substr(0, this.cPhase.length + 1);
+			if (this.cPhase == this._phase) {
+				this.mode = Doing.STATIC;
+			}
+		}
+	}
+
+	render(ctx: CanvasRenderingContext2D, ft: DOMHighResTimeStamp): void {
+		if (!this._phase && this.mode == Doing.STATIC) {
+			return;
+		}
+
+		this.doTyping(ft);
+
 		ctx.textAlign = "left";
 		ctx.textBaseline = "top";
 		ctx.font = fontify("160px", 500);
 		ctx.translate(LEFT_PADDING, TOP_PADDING);
 
-		let { width: textWidth } = ctx.measureText(this.phase);
+		let { width: textWidth } = ctx.measureText(this.cPhase);
 
 		let left;
 		if (textWidth < MAX_WIDTH) {
@@ -26,6 +96,6 @@ export class PhaseTracker extends BaseCanvasItem {
 			left = 0;
 		}
 
-		ctx.fillText(this.phase, left, 0, MAX_WIDTH);
+		ctx.fillText(this.cPhase, left, 0, MAX_WIDTH);
 	}
 }
