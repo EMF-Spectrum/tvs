9e389f7bce086973a5d470eb774a603ca065b9d1
Lex Robinson <lex@lex.me.uk>
2020-01-25 16:24:05 +0000

    Jazz up the admin hud

--- src/admin/hud.tsx
+++ src/admin/hud.tsx
@@ -3,18 +3,42 @@ import React, { useState, useEffect } from "react";
 import { callAPI } from "./api";
 import { TimerStatus } from "../types/data";
 import { CurrentTurn, CurrentPhase } from "./useGameData";
+import { time } from "core-decorators";
 
-function Moment({ duration }: { duration: number }) {
-	let [durationTXT, setDuration] = useState("");
+function getTimerStr(duration: number) {
+	let d = moment.duration(duration);
+	let mins = d
+		.minutes()
+		.toString()
+		.padStart(2, "0");
+	let secs = d
+		.seconds()
+		.toString()
+		.padStart(2, "0");
+
+	return mins + ":" + secs;
+}
+
+function Timer({ timer }: { timer: TimerStatus }) {
+	let [display, setDisplay] = useState("No Timer");
 	useEffect(() => {
-		function updoot() {
-			setDuration(moment.duration(duration).humanize());
+		if (timer.state == "hidden") {
+			setDisplay("-");
+			return;
+		} else if (timer.state == "paused") {
+			setDisplay(getTimerStr(timer.timeLeft));
+			return;
 		}
+		function updoot() {
+			if (timer.state != "running") return;
+			setDisplay(getTimerStr(timer.endTime - Date.now()));
+		}
+		updoot();
 		let handle = setInterval(updoot, 1000);
 		return () => clearInterval(handle);
-	}, []);
+	}, [timer]);
 
-	return <>{durationTXT}</>;
+	return <>{display}</>;
 }
 
 interface HUDProps {
@@ -50,6 +74,7 @@ export function HUD({ timer, currentTurn, currentPhase }: HUDProps) {
 					fontSize: "48px",
 					padding: "0 30px",
 					borderRight: "2px solid black",
+					flexGrow: 1,
 				}}
 			>
 				{currentPhase ? currentPhase.label : "Game Setup"}
@@ -61,17 +86,12 @@ export function HUD({ timer, currentTurn, currentPhase }: HUDProps) {
 					color: timer.state == "paused" ? "red" : undefined,
 					marginRight: "30px",
 					borderRight: "2px solid black",
+					flexBasis: "4.3em",
+					textAlign: "center",
+					fontFamily: "Roboto Mono",
 				}}
 			>
-				{timer.state == "hidden"
-					? "No Timer"
-					: moment
-							.duration(
-								timer.state == "paused"
-									? timer.timeLeft
-									: timer.endTime - Date.now(),
-							)
-							.humanize()}
+				<Timer timer={timer} />
 			</div>
 			{showNextTurn ? (
 				<button
@@ -84,8 +104,9 @@ export function HUD({ timer, currentTurn, currentPhase }: HUDProps) {
 							callAPI("advancePhase");
 						}
 					}}
+					style={{ width: "10em" }}
 				>
-					{!currentTurn ? "Start Game" : "Next Turn"}
+					{!currentTurn ? "Start Game" : "Next"}
 				</button>
 			) : (
 				<button
@@ -98,8 +119,9 @@ export function HUD({ timer, currentTurn, currentPhase }: HUDProps) {
 							callAPI("pause");
 						}
 					}}
+					style={{ width: "10em" }}
 				>
-					{timer.state == "paused" ? "unpause" : "pause"}
+					{timer.state == "paused" ? "Resume" : "Pause"}
 				</button>
 			)}
 		</div>
