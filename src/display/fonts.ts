d27f53a8e2c4795fd9cfed742584a35e80919a16
Lex Robinson <lex@lex.me.uk>
2021-06-05 18:33:29 +0100

    Make roboto a first class font

--- src/display/fonts.ts
+++ src/display/fonts.ts
@@ -1,10 +1,19 @@
-const FONT = "Orbitron";
+const PRIMARY_FONT = "Orbitron";
+const SECONDARY_FONT = "Roboto Mono";
 export type OrbitronWeight = 400 | 500 | 700 | 900;
-const EXPECTED_WEIGHTS: OrbitronWeight[] = [400, 500, 700, 900];
+export type RobotoWeight = 400
+const PRIMARY_WEIGHTS: OrbitronWeight[] = [400, 500, 700, 900];
+const SECONDARY_WEIGHTS: RobotoWeight[] = [400]
 
 // WebFont.load
-export function fontify(size: string, weight: OrbitronWeight = 400): string {
-	return `${weight} ${size} ${FONT}`;
+export function fontify(size: string, weight: OrbitronWeight = 400, primary=true): string {
+	let font: string;
+	if (primary) {
+		font = PRIMARY_FONT
+	} else {
+		font = SECONDARY_FONT
+	}
+	return `${weight} ${size} ${font}`;
 }
 
 export function loadFonts(): Promise<void> {
@@ -19,8 +28,8 @@ export function loadFonts(): Promise<void> {
 			},
 			google: {
 				families: [
-					`${FONT}:${EXPECTED_WEIGHTS.join(",")}`,
-					"Roboto+Mono",
+					PRIMARY_FONT + ":" + PRIMARY_WEIGHTS.join(","),
+					SECONDARY_FONT + ":" + SECONDARY_WEIGHTS.join(",")
 				],
 			},
 		});
